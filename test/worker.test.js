6ccad1f076fba295e9c34284c4a4b8b00bfda8fa
Elliot Dobson <elliot.dobson@koordinates.com>
2024-05-03 09:49:17 +1200

    Add 'cf-ray' request header as 'request_id' to all logs if present

--- test/worker.test.js
+++ test/worker.test.js
@@ -15,19 +15,21 @@ test('toLoki', t => {
   t.is(values[0][0].length, 19, 'should be nanoseconds since epoch')
   t.is(values[0][0], '1587058642005000000')
   // @ts-expect-error
-  t.is(values[0][1], JSON.stringify({ ...events[0]?.event?.request, level: 'request' }))
+  t.is(values[0][1], JSON.stringify({ ...events[0]?.event?.request, level: 'request', request_id: '57d55f210d7b95f3' }))
 
   t.is(values[1][0], '1587058642006000000')
   t.is(values[1][1], JSON.stringify({
     msg: 'string passed to console.log()',
     args: [99, { woo: 'haa' }],
-    level: 'log'
+    level: 'log',
+    request_id: '57d55f210d7b95f3'
   }))
 
   t.is(values[2][0], '1587058642007000000')
   t.is(values[2][1], JSON.stringify({
     msg: 'Threw a sample exception',
     name: 'Error',
-    level: 'fatal'
+    level: 'fatal',
+    request_id: '57d55f210d7b95f3'
   }))
 })
